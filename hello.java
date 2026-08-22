41bd8e8e59b4fda543e02fcdf67e6f80da235925
Apoorva Abha <apoorvaabha3@gmail.com>
2026-08-22 14:19:39 +0530

    Add initial Java program

---
+++ hello.java
@@ -0,0 +1,9 @@
+// package firstprogram;
+
+public class hello {
+    public static void main(String[] args) {
+      //  System.out.println("Hello!, Internshala");
+    int number = 85;
+     System.out.println(number); 
+       }
+}
