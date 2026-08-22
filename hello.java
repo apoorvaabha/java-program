157404c74bb9603f57d0468ff16a8e39fcb5bcc2
Apoorva Abha <apoorvaabha3@gmail.com>
2026-08-22 14:25:56 +0530

    Update Java program

--- hello.java
+++ hello.java
@@ -5,7 +5,10 @@ public class hello {
       //  System.out.println("Hello!, Internshala");
     int number = 85;
     int number2 = 90;
+    int sum = number + number2;
      System.out.println(number); 
      System.out.println(number2);
+        System.out.println(sum);
+    
        }
 }
