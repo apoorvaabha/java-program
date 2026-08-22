abe258eadf00dd5fe126086381c8c8aee857c292
Apoorva Abha <apoorvaabha3@gmail.com>
2026-08-22 14:23:41 +0530

    update javaprogram

--- hello.java
+++ hello.java
@@ -4,6 +4,8 @@ public class hello {
     public static void main(String[] args) {
       //  System.out.println("Hello!, Internshala");
     int number = 85;
+    int number2 = 90;
      System.out.println(number); 
+     System.out.println(number2);
        }
 }
