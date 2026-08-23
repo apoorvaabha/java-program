fa7ea77c41e159ee23047b936f32f075bb6fba20
Apoorva Abha <apoorvaabha3@gmail.com>
2026-08-23 19:32:45 +0530

    increment and decrement

---
+++ incrementanddecrement.java
@@ -0,0 +1,10 @@
+public class incrementanddecrement {
+    public static void main(String[] args) {
+        int x = 50;
+        ++x; // Incrementing x by 1
+        System.out.println("After incrementing, x = " + x);
+        --x; // Decrementing x by 1
+        System.out.println("After decrementing, x = " + x);
+    }
+    
+}
