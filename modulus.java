2fa4bd3caee546821a310887f140cb4282299717
Apoorva Abha <apoorvaabha3@gmail.com>
2026-08-23 19:27:50 +0530

    modulus.java

---
+++ modulus.java
@@ -0,0 +1,9 @@
+public class modulus {
+    public static void main(String[] args) {
+        int number1 = 10;
+        int number2 = 3;
+        int modulus = number1 % number2;
+        System.out.println(modulus);
+    }   
+    
+}
