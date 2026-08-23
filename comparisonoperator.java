095598a1531e6653b2663e3383a9799bca7dad99
Apoorva Abha <apoorvaabha3@gmail.com>
2026-08-23 20:16:09 +0530

    comparison operator

---
+++ comparisonoperator.java
@@ -0,0 +1,17 @@
+public class comparisonoperator {
+    public static void main(String[] args) {
+        int number1 = 20;
+        int number2 = 20;
+
+        boolean result;
+        result = (number1 == number2);
+        System.out.println(result);
+        //!=
+        result = (number1 != number2);
+        System.out.println(result);
+        //<> <= >=
+        result = (number1 < number2);
+        System.out.println(result);
+    } 
+
+}
