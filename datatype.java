d89475455e4da960659f9d2591978de048348ab8
Apoorva Abha <apoorvaabha3@gmail.com>
2026-08-23 16:42:06 +0530

    adding new data type

---
+++ datatype.java
@@ -0,0 +1,28 @@
+
+public class datatype {
+
+    public static void main(String[] args) {
+
+    byte num = 100;
+    System.out.println(num);
+
+    long num2=1234567;
+    System.out.println(num2);
+
+    float num3=45.6789f;
+    System.out.println(num3);
+
+    double num4 = 345.678905;
+    System.out.println(num4);
+
+    char c = 'x';
+    System.out.println(c);
+
+    boolean bool = true;
+    System.out.println(bool);
+
+    boolean bool2 = false;
+    System.out.println(bool2);
+    
+    }
+}
