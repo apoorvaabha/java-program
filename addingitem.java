b3945c1cff6586e4811b5024a34ae5f5674cf5f4
Apoorva Abha <apoorvaabha3@gmail.com>
2026-08-23 17:18:50 +0530

    cost of items

---
+++ addingitem.java
@@ -0,0 +1,11 @@
+public class addingitem {
+    public static void main(String[] args) {
+        double item1 = 12.50;
+        double item2 = 99.00;
+        double item3 = 24.75;
+        double sum = item1 + item2 + item3;
+        
+        System.out.println("The total cost of the items is Rs " + sum);
+    }
+}
+
