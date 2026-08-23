ab4f6873f49f59de5e41eb7c9dff5a971089bd47
Apoorva Abha <apoorvaabha3@gmail.com>
2026-08-24 00:23:44 +0530

    eligibilityforscholarshi

---
+++ eligibilityforscholarship.java
@@ -0,0 +1,12 @@
+public class eligibilityforscholarship {
+    
+    public static void main(String[] args) 
+    {
+      double percentage = 90;
+       double attendance = 85;
+       boolean result = (percentage >= 90) && (attendance >= 85);
+       System.out.println("scholarship eligibility: " + result);
+
+        
+    }
+}
