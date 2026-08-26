1046b357cc4a72f153a5179e5e242139db357018
Apoorva Abha <apoorvaabha3@gmail.com>
2026-08-26 21:19:41 +0530

    if and if else conditional statements

---
+++ ifandifelse.java
@@ -0,0 +1,13 @@
+public class ifandifelse {
+    public static void main(String[] args) {
+        int num = 47;
+        //even
+        if (num % 2 == 0) {
+            System.out.println(num + " is even.");
+        } 
+        // odd
+        else {
+            System.out.println(num + " is odd.");
+
+    }
+    }}
