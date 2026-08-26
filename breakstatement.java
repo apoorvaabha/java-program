ea4bf078dbdac2f44d33cf57334dc5d742d533a7
Apoorva Abha <apoorvaabha3@gmail.com>
2026-08-26 22:05:16 +0530

    break statements

---
+++ breakstatement.java
@@ -0,0 +1,12 @@
+public class breakstatement {
+    
+    public static void main(String[] args) {
+        int i;
+        for (i = 1; i <= 10; i++) {
+            if (i == 7) 
+                break; // stop the loop
+            System.out.println(i);
+        //<7
+        }
+    }
+}
