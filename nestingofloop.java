cba5f577ccbaf69102580dd1109bc6b284d523bc
Apoorva Abha <apoorvaabha3@gmail.com>
2026-08-26 21:55:23 +0530

    nesting of loop

---
+++ nestingofloop.java
@@ -0,0 +1,18 @@
+public class nestingofloop {
+    public static void main(String[] args) {
+   
+    //*****
+    //*****
+    //*****
+    //***** 
+    //*****
+        for (int i = 1; i <= 5; i++) {     // outer loop
+            for (int j = 1; j <= 5; j++) {  // inner loop
+                System.out.println("*");
+            }
+                System.out.println();
+                // i =2,3
+            }
+        }
+    }
+
