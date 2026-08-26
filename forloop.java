5772f832611ce1cb3b121bf3965da8b6ea3648ef
Apoorva Abha <apoorvaabha3@gmail.com>
2026-08-26 21:44:22 +0530

    for loop

---
+++ forloop.java
@@ -0,0 +1,12 @@
+public class forloop {
+    public static void main(String[] args) {
+        //sum of first 10 natural numbers
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+         //body of for loop
+            sum = sum + i;
+        }
+       System.out.println("Sum ="+ sum);
+        }
+    }
+
