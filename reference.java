b248e2965648456e5fa94b8a2f364ac975efbc56
Apoorva Abha <apoorvaabha3@gmail.com>
2026-08-30 21:46:38 +0530

    reference(==)method

---
+++ reference.java
@@ -0,0 +1,10 @@
+public class reference {
+    public static void main(String[] args) {
+        String str1 = "James";
+        String str2 = "Smith";
+        // reference comparison
+        boolean b = (str1 == str2); // true/false
+        System.out.println(b);
+    }
+    
+}
