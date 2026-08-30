19e32e20763a2ede0712bde61db411c86449d943
Apoorva Abha <apoorvaabha3@gmail.com>
2026-08-30 21:39:30 +0530

    string comparison equal() method

---
+++ Stringcomparison.java
@@ -0,0 +1,10 @@
+public class stringcomparison {
+        public static void main(String[] args) {
+        String str1 = "James";
+        String str2 = "Smith";
+        // equals() method
+        boolean b = str1.equals(str2); // true/false
+        System.out.println(b);
+    }
+    
+}
