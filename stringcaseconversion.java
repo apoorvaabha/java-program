3c2405fc65779f079e6a91c7b1aaf5921802c029
Apoorva Abha <apoorvaabha3@gmail.com>
2026-08-30 21:57:02 +0530

    string case conversion

---
+++ stringcaseconversion.java
@@ -0,0 +1,11 @@
+public class stringcaseconversion {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        // toUpperCase() method
+        String upperStr = str.toUpperCase();
+        System.out.println(upperStr);
+        // toLowerCase() method
+        String lowerStr = str.toLowerCase();
+        System.out.println(lowerStr);
+    }   
+}
