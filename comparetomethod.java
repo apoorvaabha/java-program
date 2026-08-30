dc300a5facf5bb8d834d3caa567b832d201101d7
Apoorva Abha <apoorvaabha3@gmail.com>
2026-08-30 21:43:49 +0530

    compare to method

---
+++ comparetomethod.java
@@ -0,0 +1,9 @@
+public class comparetomethod {
+    public static void main(String[] args) {
+        String str1 = "James";
+        String str2 = "Smith";
+        // compareTo() method
+        int result = str1.compareTo(str2); // -1, 0, 1
+        System.out.println(result);
+    }
+}
