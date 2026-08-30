f519cfaa5ab5cc064c691b948d4b037cf5412f48
Apoorva Abha <apoorvaabha3@gmail.com>
2026-08-30 21:58:46 +0530

    Trim operation

---
+++ trimoperation.java
@@ -0,0 +1,8 @@
+public class trimoperation {
+    public static void main(String[] args) {
+        String str = "   Hello, World!   ";
+        // trim() method
+        String trimmedStr = str.trim();
+        System.out.println(trimmedStr);
+    }
+}
