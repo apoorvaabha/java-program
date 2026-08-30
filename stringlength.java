a6bdaaec7b6c9e029f076d6d576ef7eb2337a103
Apoorva Abha <apoorvaabha3@gmail.com>
2026-08-30 21:19:28 +0530

    string length

---
+++ stringlength.java
@@ -0,0 +1,10 @@
+public class stringlength {
+    public static void main(String[] args) {
+        String firstname = "Hello";
+        String lastname = "World!";
+        // string length method
+        int len = firstname.length();
+        System.out.println(len);
+        
+    }
+}
